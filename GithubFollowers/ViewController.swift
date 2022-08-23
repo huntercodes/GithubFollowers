c9082e4889b91eeb3668903a1c07bead2985fe4d
Hunter Downey <hdowneymac@gmail.com>
2022-08-23 14:26:16 -0500

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by hunter downey on 8/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubFollowersUITests/GithubFollowersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GithubFollowersUITestsLaunchTests.swift
+//  GithubFollowersUITests
+//
+//  Created by hunter downey on 8/23/22.
+//
+
+import XCTest
+
+class GithubFollowersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
