4e93356ad1a4e64e891cbf7276aaf093a509c703
Hunter Downey <hdowneymac@gmail.com>
2022-09-01 14:50:49 -0500

    Secondary Title Label

---
+++ GithubFollowers/Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GithubFollowers
+//
+//  Created by hunter downey on 9/1/22.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        config()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        config()
+    }
+    
+    private func config() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
