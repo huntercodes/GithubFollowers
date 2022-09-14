34d4ce69e040fe43a0709aeb8512998b0ba99972
Hunter Downey <hdowneymac@gmail.com>
2022-09-14 02:30:37 -0500

    UIHelper Fix

--- GithubFollowers/Utilities/UIHelper.swift
+++ GithubFollowers/Utilities/UIHelper.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-struct UIHelper {
+enum UIHelper {
     
     static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
         let width = view.bounds.width
