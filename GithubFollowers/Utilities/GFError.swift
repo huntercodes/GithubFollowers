c6dca575925ba7d553e55bb5a9969e304523b6a3
Hunter Downey <hdowneymac@gmail.com>
2022-10-17 15:24:19 -0500

    Error Messages

--- GithubFollowers/Utilities/GFError.swift
+++ GithubFollowers/Utilities/GFError.swift
@@ -9,11 +9,11 @@ import Foundation
 
 enum GFError: String, Error {
     
-    case invalidUsername = "This username created an invalid request. Please try again."
-    case unableToComplete = "Unable to complete your request. Please check your internet connection"
-    case invalidResponse = "Invalid response from the server. Please try again."
-    case invalidData = "The data received from the server was invalid. Please try again."
-    case unableToFavorite = "There was an error favoriting this user. Please try again."
+    case invalidUsername = "This username created an invalid request. Please try again!"
+    case unableToComplete = "Unable to complete your request. Please check your internet connection!"
+    case invalidResponse = "Invalid response from the server. Please try again!"
+    case invalidData = "The data received from the server was invalid. Please try again1"
+    case unableToFavorite = "There was an error favoriting this user. Please try again!"
     case alreadyInFavorites = "You've already favorited this user. You must REALLY like them!"
     
 }
