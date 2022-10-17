d18d7d6e9478d68e5ea20d94f5c31f80ff13a280
Hunter Downey <hdowneymac@gmail.com>
2022-10-17 15:26:22 -0500

    Action Button

--- GithubFollowers/Views/ViewControllers/GFAlertVC.swift
+++ GithubFollowers/Views/ViewControllers/GFAlertVC.swift
@@ -12,7 +12,7 @@ class GFAlertVC: UIViewController {
     let containerView = GFAlertContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
-    let actionButton = GFButton(color: .systemPink, title: "Ok", systemImageName: "checkmark.circle")
+    let actionButton = GFButton(color: .systemPink, title: "Alright", systemImageName: "checkmark.circle")
     
     var alertTitle: String?
     var message: String?
